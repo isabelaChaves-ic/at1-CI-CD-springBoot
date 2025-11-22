ce834135546b0ce145d2b4d4f89a96ba1d446c00
Isabela Chaves <isabelachaves074@outlook.com.br>
2025-11-22 17:01:33 -0300

    fix: cria classe principal da aplicação que estava faltando

---
+++ src/main/java/com/isabela/at1_t2_tarefas/At1T2TarefasApplication.java
@@ -0,0 +1,14 @@
+package com.isabela.at1_t2_tarefas;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class At1T2TarefasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(At1T2TarefasApplication.class, args);
+    }
+
+}
